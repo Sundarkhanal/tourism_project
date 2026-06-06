fec4e130805389b30cd50f8dde4ac155dc65a5e8
Sundar Khanal <sundarkhanal62@gmail.com>
2026-06-07 00:23:10 +0545

    feat: added static, error-handler middleware and body parser

--- server/src/config/express.config.js
+++ server/src/config/express.config.js
@@ -1,9 +1,34 @@
 const express = require("express")
 const router = require("../router/router")
+const errorHanlder = require("../middlewares/error-handler.middleware")
 
 const app = express()
 
+// body parser
+app.use(express.json({
+    limit: "5mb"
+}))
+app.use(express.urlencoded({
+    limit: "5mb"
+}))
+
+//static middleware
+app.use("assets/", express.static('./public/uploads/'))
+
 app.use("/api/v1", router)
+
+
+//route globally undefined path or routes
+app.use((req, res, next) => {
+    next({
+        code: 404,
+        details: null,
+        message: "Not Found",
+        status:"NOT_FOUND_ERR"
+    })
+})
+
+app.use(errorHanlder)
 
 
 
