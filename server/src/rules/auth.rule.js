77436e5031f659c611398e497d8016d3d069ba9a
Sundar Khanal <sundarkhanal62@gmail.com>
2026-06-13 22:19:50 +0545

    Backend Progress

--- server/src/rules/auth.rule.js
+++ server/src/rules/auth.rule.js
@@ -1,12 +1,24 @@
 const Joi = require("joi");
+const genderPattern = /^(male|female|others)/
+const passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[\d])(?=.*[\W_-]).{8,25}$/
 
 const RegisterDTO = Joi.object({
     name: Joi.string().min(2).max(50).required(),
     email: Joi.string().email().required(),
-    password: Joi.string().min(8).max(25).required().messages({
-        "any.required":"Password is Compulsory"
+    password: Joi.string().regex(passwordPattern).required().messages({
+        "any.required":"Password is Compulsory",
+        'string.pattern.base': "Password must have 1 Lowercase Character, 1 Uppercase Character, 1 number and a special letter"
     }),
-    confirmPassword: Joi.ref('password'),
+    confirmPassword: Joi.string().valid(Joi.ref('password')).messages({
+        "any.only":"Password and Confirm Password must match"
+    }),
+    phone:Joi.string().required().messages({
+        "string.empty": "Phone is not allowed to by empty"
+    }),
+    gender:Joi.string().regex(genderPattern).required().messages({
+        "string.pattern.base":"Gender must be either Male or Female or Others"
+    }),
+    address:Joi.string().optional().allow(null, "").default(null)
 })
 
 module.exports = RegisterDTO
