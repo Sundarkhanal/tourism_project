968c3db54b75c38c3c98cfde58ad73582b93500e
Samir Manandhar <samir.manandhar24@apexcollege.edu.np>
2026-07-12 19:34:35 +0545

    Seperated Routing of Admin and Client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,16 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/authentication/Login";
-import Register from "./pages/authentication/Register";
-import Profile from "./pages/profile/Profile";
-import News from "./pages/News";
-import Blogs from "./pages/blog/Blogs";
-import Destinations from "./pages/Destinations";
-import Layout from "./pages/components/Layout";
-import './App.css';
-import ForgotPassword from "./pages/authentication/forgotpassword";
-import ResetPassword from "./pages/authentication/resetpassword";
-import ActivateAccount from "./pages/authentication/ActivateAccount";
-import DestinationDetails from "./pages/destination/DestinationDetails";
+import { BrowserRouter } from "react-router-dom";
+import ClientRoutes from "./routes/ClientRoute";
+import AdminRoutes from "./routes/AdminRoute";
 
+import "./App.css";
 
 function App() {
- 
-  return (
-    <BrowserRouter>
-    <Routes>
-      <Route element={<Layout />}>
-
-        <Route path="/" element={<Home/>} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/news" element={<News/>} />
-        <Route path="/blogs" element={<Blogs/>} />
-        <Route path="/destinations" element={<Destinations/>} />
-        <Route path="/destination/:id" element={<DestinationDetails />} />
-
-      </Route>
-      <Route path="/register" element={<Register/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/forgotpassword" element={<ForgotPassword/>} />
-      <Route path="/reset-password" element={<ResetPassword/>} />
-      <Route path="/activate-account" element={<ActivateAccount/>} />
-      
-    </Routes>
-    </BrowserRouter>
-  )
+    return (
+        <BrowserRouter>
+            <ClientRoutes />
+            <AdminRoutes />
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ client/src/routes/ClientRoute.jsx
@@ -0,0 +1,38 @@
+import { Routes, Route } from "react-router-dom";
+
+import Layout from "../pages/components/Layout";
+import Home from "../pages/Home";
+import Profile from "../pages/profile/Profile";
+import News from "../pages/News";
+import Blogs from "../pages/blog/Blogs";
+import Destinations from "../pages/Destinations";
+import DestinationDetails from "../pages/destination/DestinationDetails";
+
+import Login from "../pages/authentication/Login";
+import Register from "../pages/authentication/Register";
+import ForgotPassword from "../pages/authentication/ForgotPassword";
+import ResetPassword from "../pages/authentication/ResetPassword";
+import ActivateAccount from "../pages/authentication/ActivateAccount";
+
+export default function ClientRoutes() {
+    return (
+        <Routes>
+
+            <Route element={<Layout />}>
+                <Route path="/" element={<Home />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="/news" element={<News />} />
+                <Route path="/blogs" element={<Blogs />} />
+                <Route path="/destinations" element={<Destinations />} />
+                <Route path="/destination/:id" element={<DestinationDetails />} />
+            </Route>
+
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/forgotpassword" element={<ForgotPassword />} />
+            <Route path="/reset-password" element={<ResetPassword />} />
+            <Route path="/activate-account" element={<ActivateAccount />} />
+
+        </Routes>
+    );
+}
